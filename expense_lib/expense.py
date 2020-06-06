fd52729c9f28256bf2703bc212f386f9c2a17a7c
Sachin Sunder Sagadevan <sachsun2809@outlook.com>
2020-06-05 19:54:48 -0700

    Logging and CI updates (#4)
    * Reorganizing code
    
    * Reorganizing code
    
    * Reorganizing code
    
    * Reorganizing code
    
    * Reorganizing code
    
    * linting fixes
    
    * updating .gitignore
    
    * refactoring code
    
    * updating tests
    
    * refactoring code [#2]

---
+++ expense_lib/expense.py
@@ -0,0 +1,62 @@
+from datetime import datetime
+
+
+class Expense:
+
+    def __init__(self):
+        self.expense_date = None
+        self.expense_amount = 0
+        self.iscredit_bool = None
+        self.expense_description = ""
+        self.expense_address = ""
+        self.reference_number = ""
+        self.expense_category = ""
+
+    @classmethod
+    def from_dict(cls, input_map):
+        expense = cls()
+        expense.expense_amount = input_map.get("amount", 0)
+        expense.expense_date = input_map.get("date")
+        expense.iscredit_bool = input_map.get("is_credit")
+        expense.expense_description = input_map.get("description", "")
+        expense.expense_address = input_map.get("address", "")
+        expense.reference_number = input_map.get("ref_num", "")
+        expense.expense_category = input_map.get("category", "")
+        return expense
+
+    @property
+    def date(self):
+        return self.expense_date.strftime("%m/%d/%Y")
+
+    @property
+    def amount(self):
+        return self.expense_amount
+
+    @property
+    def iscredit(self):
+        return self.iscredit_bool
+
+    @property
+    def map(self):
+        if self.iscredit_bool:
+            amount = self.amount * -1
+            type = "CREDIT"
+        else:
+            amount = self.amount
+            type = "DEBIT"
+        output_map = {
+            "Date": self.date.strip(),
+            "Amount": amount,
+            "Reference": self.reference_number.strip(),
+            "Category": self.expense_category.strip(),
+            "Address": self.expense_address.strip(),
+            "Description": self.expense_description.strip(),
+            "Type": type,
+        }
+        return output_map
+
+    def __eq__(self, other):
+        return self.expense_date == other.expense_date
+
+    def __lt__(self, other):
+        return self.expense_date < other.expense_date
---
+++ expense_lib/normalizer.py
@@ -0,0 +1,78 @@
+from datetime import datetime
+from .constants import BOFA
+from .constants import AMEX
+from .constants import AMEX_YTD
+from.constants import DISCOVER
+
+
+def get_expense_type(header_list):
+    if set(header_list) == set(BOFA):
+        expense_type = "bofa"
+    elif set(header_list) == set(AMEX):
+        expense_type = "amex"
+    elif set(header_list) == set(DISCOVER):
+        expense_type = "discover"
+    elif set(header_list) == set(AMEX_YTD):
+        expense_type = "amex_ytd"
+    else:
+        expense_type = None
+    return expense_type
+
+
+def bofa_normalizer(input_map):
+    output_map = dict()
+    tmp_amount = float(input_map.get("Amount", 0))
+    output_map["amount"] = abs(tmp_amount)
+    date_time_obj = datetime.strptime(input_map.get("Posted Date"), "%m/%d/%Y")
+    output_map["date"] = date_time_obj
+    output_map["is_credit"] = tmp_amount > 0
+    output_map["description"] = input_map.get("Payee")
+    output_map["address"] = input_map.get("Address")
+    output_map["ref_num"] = input_map.get("Reference Number")
+    output_map["category"] = None
+    return output_map
+
+
+def amex_normalizer(input_map):
+    output_map = dict()
+    tmp_amount = float(input_map.get("Amount", 0))
+    output_map["amount"] = abs(tmp_amount)
+    date_time_obj = datetime.strptime(input_map.get("Date"), "%m/%d/%y")
+    output_map["date"] = date_time_obj
+    output_map["is_credit"] = tmp_amount < 0
+    output_map["description"] = input_map.get("Description")
+    output_map["ref_num"] = input_map.get("Reference", "")
+    output_map["category"] = input_map.get("Category", "")
+    return output_map
+
+
+def amex_ytd_normalizer(input_map):
+    output_map = dict()
+    tmp_amount = float(input_map.get("Amount #", 0))
+    output_map["amount"] = abs(tmp_amount)
+    date_time_obj = datetime.strptime(input_map.get("Date"), "%m/%d/%y")
+    output_map["date"] = date_time_obj
+    output_map["is_credit"] = tmp_amount < 0
+    output_map["description"] = input_map.get("Description")
+    output_map["ref_num"] = input_map.get("Reference", "")
+    output_map["category"] = input_map.get("Category", "")
+    return output_map
+
+def discover_normalizer(input_map):
+    output_map = dict()
+    tmp_amount = float(input_map.get("Amount", 0))
+    output_map["amount"] = abs(tmp_amount)
+    date_time_obj = datetime.strptime(input_map.get("Post Date"), "%m/%d/%Y")
+    output_map["date"] = date_time_obj
+    output_map["is_credit"] = tmp_amount < 0
+    output_map["description"] = input_map.get("Description")
+    output_map["category"] = input_map.get("Category")
+    return output_map
+
+
+normalizer_map = {
+    "amex_ytd": amex_normalizer,
+    "amex": amex_normalizer,
+    "discover": discover_normalizer,
+    "bofa": bofa_normalizer,
+}
